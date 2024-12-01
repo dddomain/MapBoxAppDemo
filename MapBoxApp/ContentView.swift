8a57deb22e38ab5fc8259d7badfb16a6aeed9ea8
dddomain <xaxa.reira2306@icloud.com>
2024-12-01 21:49:50 +0900

    Initial Commit

---
+++ MapBoxApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapBoxApp
+//
+//  Created by 小原大輔 on 2024/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapBoxApp/MapBoxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapBoxAppApp.swift
+//  MapBoxApp
+//
+//  Created by 小原大輔 on 2024/12/01.
+//
+
+import SwiftUI
+
+@main
+struct MapBoxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
